efd9d8b18b8bd1a284cd2e32a635e25646d51070
Eugene Khashin <adastreamer@gmail.com>
2015-10-05 00:48:24 +0300

    dummy ios map

---
+++ ios/RNYMapsManager/RNYMapsManager.h
@@ -0,0 +1,15 @@
+//
+//  RNYMapsManager.h
+//  Online-Lab
+//
+
+#import <UIKit/UIKit.h>
+#import <sys/utsname.h>
+#import "RCTViewManager.h"
+
+#import "RCTBridgeModule.h"
+#import "YandexMapKit.h"
+
+@interface RNYMapsManager : RCTViewManager
+
+@end
