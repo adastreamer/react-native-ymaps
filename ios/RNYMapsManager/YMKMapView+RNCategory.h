6376d832041bbdeef2a8c6171caaaa9807e5cb8f
Lydia Khashina <adatheleader@gmail.com>
2015-10-11 18:20:46 +0300

    RNCategory.m added

---
+++ ios/RNYMapsManager/YMKMapView+RNCategory.h
@@ -0,0 +1,13 @@
+//
+//  YMKMapView+RNCategory.h
+//  RNYMapsManager
+//
+//  Created by Лидия Хашина on 11.10.15.
+//  Copyright © 2015 Online-Lab. All rights reserved.
+//
+
+#import "YMKMapView.h"
+
+@interface YMKMapView (RNCategory)
+
+@end
---
+++ ios/RNYMapsManager/YMKMapView+RNCategory.m
@@ -0,0 +1,14 @@
+//
+//  YMKMapView+RNCategory.m
+//  RNYMapsManager
+//
+//  Created by Лидия Хашина on 11.10.15.
+//  Copyright © 2015 Online-Lab. All rights reserved.
+//
+
+#import "YMKMapView+RNCategory.h"
+#import <UIKit/UIKit.h>
+
+@implementation YMKMapView (RNCategory)
+
+@end
